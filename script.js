1c4a6e1eb6feeef784944ad52d635ab00f085c31
Arnab-1473 <arban.nilr.1473@gmail.com>
2023-02-26 21:58:48 +0530

    updated

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here
+const input = document.getElementById("newTodoInput");
+const button = document.getElementById("addTodoBtn");
+const todoList = document.getElementById("todoList");
+
+function addTodo(){
+    const newTodo = input.ariaValueMax.trim();
+    if(newTodo !== ""){
+        const listItem = document.createElement("li");
+        listItem.innerText = newTodo;
+        todolist.appendChild(listItem);
+        input.value = "";
+    }
+}
+
+button.addEventListener("click", addTodo);
 
